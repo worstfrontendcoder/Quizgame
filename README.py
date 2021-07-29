4882b0688eea6c2056aa8dcbef1734f170140c1c
Code.py <87883481+NafizTech@users.noreply.github.com>
2021-07-28 23:57:29 -0500

    Create README.py

---
+++ README.py
@@ -0,0 +1,4 @@
+# Quizgame
+A general knowledge trivia game made with python!
+A quiz is a form of game or mind sport in which players attempt to answer questions correctly about a certain or variety of subjects. Quizzes can be used as a brief assessment in education and similar fields to measure growth in knowledge, abilities, or skills. They can also be televised for entertainment purposes, often in a game show format.
+Make sure to get the answers right and get 80% score to beat the game btw these are just simple general knowlegde question!
